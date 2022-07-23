177a9f93b55a5542da68b3728b15bb2b35e7b50f
IrinaVerbitskaja <mgmt-m@yandex.ru>
2022-07-23 20:57:54 +0300

    моки

--- project/src/components/film-list.tsx
+++ project/src/components/film-list.tsx
@@ -2,7 +2,7 @@ import {Films, Film} from '../types/film';
 import FilmCard from './film/film-card';
 
 function FilmCardList (props: Films): JSX.Element {
-    return {props.map((film: Film) => (
+    return {props.map((film) => (
     <FilmCard {...film}/>)
   };
 }
